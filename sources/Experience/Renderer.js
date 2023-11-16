80254fec9c7e2d7046411bacb62783d7c60ab6ad
Néo <miannayneo@gmail.com>
2023-11-16 16:01:08 +0100

    first commit

---
+++ sources/Experience/Renderer.js
@@ -0,0 +1,145 @@
+import * as THREE from 'three'
+import Experience from './Experience.js'
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js'
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js'
+
+export default class Renderer
+{
+    constructor(_options = {})
+    {
+        this.experience = new Experience()
+        this.config = this.experience.config
+        this.debug = this.experience.debug
+        this.stats = this.experience.stats
+        this.time = this.experience.time
+        this.sizes = this.experience.sizes
+        this.scene = this.experience.scene
+        this.camera = this.experience.camera
+
+        // Debug
+        if(this.debug)
+        {
+            this.debugFolder = this.debug.addFolder('renderer')
+        }
+        
+        this.usePostprocess = false
+
+        this.setInstance()
+        this.setPostProcess()
+    }
+
+    setInstance()
+    {
+        this.clearColor = '#1e3342'
+
+        // Renderer
+        this.instance = new THREE.WebGLRenderer({
+            // alpha: false,
+            antialias: true
+        })
+        this.instance.domElement.style.position = 'absolute'
+        this.instance.domElement.style.top = 0
+        this.instance.domElement.style.left = 0
+        this.instance.domElement.style.width = '100%'
+        this.instance.domElement.style.height = '100%'
+
+        this.instance.setClearColor(this.clearColor, 1)
+        this.instance.setSize(this.config.width, this.config.height)
+        this.instance.setPixelRatio(this.config.pixelRatio)
+
+        this.instance.physicallyCorrectLights = false
+        this.instance.outputColorSpace = THREE.SRGBColorSpace
+        this.instance.toneMapping = THREE.ACESFilmicToneMapping
+        this.instance.toneMappingExposure = 1
+        
+        this.instance.shadowMap.enabled = true
+        this.instance.shadowMap.type = THREE.PCFSoftShadowMap
+
+        this.context = this.instance.getContext()
+
+        // Add stats panel
+        if(this.stats)
+        {
+            this.stats.setRenderPanel(this.context)
+        }
+        
+        // Debug
+        if(this.debug)
+        {
+            
+        }
+    }
+
+    setPostProcess()
+    {
+        this.postProcess = {}
+
+        /**
+         * Render pass
+         */
+        this.postProcess.renderPass = new RenderPass(this.scene, this.camera.instance)
+
+        /**
+         * Effect composer
+         */
+        this.renderTarget = new THREE.WebGLRenderTarget(
+            this.config.width,
+            this.config.height,
+            {
+                generateMipmaps: false,
+                minFilter: THREE.LinearFilter,
+                magFilter: THREE.LinearFilter,
+                format: THREE.RGBFormat,
+                encoding: THREE.sRGBEncoding,
+                samples: 2
+            }
+        )
+        this.postProcess.composer = new EffectComposer(this.instance, this.renderTarget)
+        this.postProcess.composer.setSize(this.config.width, this.config.height)
+        this.postProcess.composer.setPixelRatio(this.config.pixelRatio)
+
+        this.postProcess.composer.addPass(this.postProcess.renderPass)
+    }
+
+    resize()
+    {
+        // Instance
+        this.instance.setSize(this.config.width, this.config.height)
+        this.instance.setPixelRatio(this.config.pixelRatio)
+
+        // Post process
+        this.postProcess.composer.setSize(this.config.width, this.config.height)
+        this.postProcess.composer.setPixelRatio(this.config.pixelRatio)
+    }
+
+    update()
+    {
+        if(this.stats)
+        {
+            this.stats.beforeRender()
+        }
+
+        if(this.usePostprocess)
+        {
+            this.postProcess.composer.render()
+        }
+        else
+        {
+            this.instance.render(this.scene, this.camera.instance)
+        }
+
+        if(this.stats)
+        {
+            this.stats.afterRender()
+        }
+    }
+
+    destroy()
+    {
+        this.instance.renderLists.dispose()
+        this.instance.dispose()
+        this.renderTarget.dispose()
+        this.postProcess.composer.renderTarget1.dispose()
+        this.postProcess.composer.renderTarget2.dispose()
+    }
+}
---
+++ sources/Experience/World.js
@@ -0,0 +1,42 @@
+import * as THREE from 'three'
+import Experience from './Experience.js'
+
+export default class World
+{
+    constructor(_options)
+    {
+        this.experience = new Experience()
+        this.config = this.experience.config
+        this.scene = this.experience.scene
+        this.resources = this.experience.resources
+        
+        this.resources.on('groupEnd', (_group) =>
+        {
+            if(_group.name === 'base')
+            {
+                this.setScene()
+            }
+        })
+    }
+
+    setScene()
+    {   
+        const cube = new THREE.Mesh(
+            new THREE.BoxGeometry(1, 1, 1),
+            new THREE.MeshBasicMaterial({ color: 0xff0000 })
+        )
+        this.scene.add(cube)        
+    }
+
+    resize()
+    {
+    }
+
+    update()
+    {
+    }
+
+    destroy()
+    {
+    }
+}
